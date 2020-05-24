fd1c545b843367c2509c37c3e846e212f05779ec
jon.r.bushman@gmail.com <bushfactsanalytics@gmail.com>
2020-05-25 00:53:23 +0430

    working graphs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.express as px
 
 import pandas as pd
 
@@ -15,45 +16,33 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     dcc.Graph(id='graph-with-dropdown'),
     dcc.Dropdown(
-                id='unit',
-                options=[{'label': unitIDs["name"][i], 'value': unitIDs["id"][i]} for i in range(len(unitIDs))],
-                value='Fertility rate, total (births per woman)'
+                id='unit-name',
+                options=[{'label': unitIDs["name"][i], 'value': i} for i in range(len(unitIDs))],
+                value=43
             )
 ])
 
 
 @app.callback(
     Output('graph-with-dropdown', 'figure'),
-    [Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(dict(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
+    [Input('unit-name', 'value')])
+def update_figure(selected_df_ID):
+    selected_unitID = unitIDs.at[selected_df_ID,'id']
+    unitName = unitIDs.at[selected_df_ID,'name']
+    df = pd.read_csv('data/units/upgrades/' + str(selected_unitID) + ".csv")
+    data = [{'values': [i for i in df['count']], 'labels': [i for i in df['upgrades']], 'type': 'pie', 'title': unitName}]
 
     return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita',
-                   'range':[2.3, 4.8]},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest',
-            transition = {'duration': 500},
-        )
+        'data': data
+        # 'layout': dict(
+        #     xaxis={'type': 'log', 'title': 'GDP Per Capita',
+        #            'range':[2.3, 4.8]},
+        #     yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+        #     margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+        #     legend={'x': 0, 'y': 1},
+        #     hovermode='closest',
+        #     transition = {'duration': 500},
+        # )
     }
 
 
