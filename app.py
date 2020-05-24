ce4c81a782cb658ea37d134ccb81355f62f1fc1b
jon.r.bushman@gmail.com <bushfactsanalytics@gmail.com>
2020-05-25 02:09:51 +0430

    requirements

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
 import pandas as pd
 
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
