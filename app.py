f4ee74723044f9cc0d432776f96132bfcc075cb2
jon.r.bushman@gmail.com <bushfactsanalytics@gmail.com>
2020-05-25 02:25:07 +0430

    server attribute

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ unitIDs = pd.read_csv('data/unitIDs.csv')
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div([
     dcc.Graph(id='graph-with-dropdown'),
