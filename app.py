de823fa7a26520795058cc48db817e053daa28a8
jon.r.bushman@gmail.com <bushfactsanalytics@gmail.com>
2020-05-24 23:49:09 +0430

    graph test

--- app.py
+++ app.py
@@ -1,27 +1,60 @@
-import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
-app = dash.Dash(__name__)
-server = app.server
+import pandas as pd
+
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+unitIDs = pd.read_csv('data/unitIDs.csv')
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.H2('Hello Clarise'),
+    dcc.Graph(id='graph-with-dropdown'),
     dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['wut', 'the', 'fuh']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+                id='unit',
+                options=[{'label': unitIDs["name"][i], 'value': unitIDs["id"][i]} for i in range(len(unitIDs))],
+                value='Fertility rate, total (births per woman)'
+            )
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
 
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(
+    Output('graph-with-dropdown', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(dict(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
 
+    return {
+        'data': traces,
+        'layout': dict(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita',
+                   'range':[2.3, 4.8]},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            transition = {'duration': 500},
+        )
+    }
 
 
 if __name__ == '__main__':
