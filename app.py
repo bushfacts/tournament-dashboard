d7ba9fa63f8988b37827d67785ba3acf64396107
jon.r.bushman@gmail.com <bushfactsanalytics@gmail.com>
2020-05-25 04:35:07 +0430

    working now

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-# import plotly.express as px
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
@@ -22,7 +23,6 @@ app.layout = html.Div([
             )
 ])
 
-
 @app.callback(
     Output('graph-with-dropdown', 'figure'),
     [Input('unit-name', 'value')])
@@ -32,18 +32,9 @@ def update_figure(selected_df_ID):
     unitCount = unitIDs.at[selected_df_ID,'count']
     df = pd.read_csv('data/units/upgrades/' + str(selected_unitID) + ".csv")
     data = [{'values': [i for i in df['count']], 'labels': [i for i in df['upgrades']], 'type': 'pie', 'title': unitName + ": " + str(unitCount) + "\n"}]
-
+    fig = go.Figure()
     return {
-        'data': data
-        # 'layout': dict(
-        #     xaxis={'type': 'log', 'title': 'GDP Per Capita',
-        #            'range':[2.3, 4.8]},
-        #     yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-        #     margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-        #     legend={'x': 0, 'y': 1},
-        #     hovermode='closest',
-        #     transition = {'duration': 500},
-        # )
+        "data": data
     }
 
 
