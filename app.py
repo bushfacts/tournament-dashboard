7a4a587d84314577fb96068226022f115ace18b0
jon.r.bushman@gmail.com <bushfactsanalytics@gmail.com>
2020-05-26 22:41:03 +0430

    fixed unitUpgrade Graphs

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+import json
 
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
-unitIDs = pd.read_csv('data/units/counts.csv')
+unitStats = json.loads(open("data/unitStats.json").read())
+unitIDs = [[unit,unitStats[unit]['name']] for unit in unitStats]
+unitIDs.sort(key=lambda x:x[1]) #alphabetical sort
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -15,26 +18,47 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-dropdown'),
+    dcc.Graph(id='main-graph'),
     dcc.Dropdown(
                 id='unit-name',
-                options=[{'label': unitIDs["name"][i], 'value': i} for i in range(len(unitIDs))],
-                value=43
+                options=[{'label': unit[1], 'value': unit[0]} for unit in unitIDs],
+                value=unitIDs[0][0]
             )
 ])
 
 @app.callback(
-    Output('graph-with-dropdown', 'figure'),
+    Output('main-graph', 'figure'),
     [Input('unit-name', 'value')])
-def update_figure(selected_df_ID):
-    selected_unitID = unitIDs.at[selected_df_ID,'id']
-    unitName = unitIDs.at[selected_df_ID,'name']
-    unitCount = unitIDs.at[selected_df_ID,'count']
-    df = pd.read_csv('data/units/upgrades/' + str(selected_unitID) + ".csv")
-    data = [{'values': [i for i in df['count']], 'labels': [i for i in df['upgrades']], 'type': 'pie', 'title': unitName + ": " + str(unitCount) + "\n", 'textinfo': ('label+value')}]
+def update_figure(unitID):
     fig = go.Figure()
+    traces = []
+    unitName = unitStats[unitID]['name']
+    unitCount = int(unitStats[unitID]['count'])
+    unitRank =  unitStats[unitID]['rank']
+    # data = [{'name':unitStats[unitID]['upgrades']['name'], 'count':unitStats[unitID]['upgrades']['count']} for upgrade in unitStats[unitID]['upgrades']]
+    for type in unitStats[unitID]['upgrades']:
+        data = [{'name':upgrade, 'count':int(unitStats[unitID]['upgrades'][type][upgrade])} for upgrade in unitStats[unitID]['upgrades'][type]]
+        data.sort(reverse=True, key=lambda x:x['count'])
+        labels = [upgrade['name'] for upgrade in data]
+        values = [upgrade['count'] for upgrade in data]
+
+        traces.append({
+            'x':labels,
+            'y':values,
+            'type':'bar',
+            'name':type.title()
+        })
+
+    # df = pd.read_csv('data/units/upgrades/' + str(selected_unitID) + ".csv")
+    # data = [{'values': [i for i in df['count']], 'labels': [i for i in df['upgrades']], 'type': 'pie', 'title': unitName + ": " + str(unitCount) + "\n", 'textinfo': ('label+value')}]
+
     return {
-        "data": data
+        'data': traces,
+        'layout': {
+            'yaxis': {'range': [0,unitCount]},
+            'transition': {'duration': 500},
+            'title': unitName
+        }
     }
 
 
